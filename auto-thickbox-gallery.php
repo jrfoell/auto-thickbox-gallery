d6163f1a0537cec59f120bf3429599b227b45489
Justin Foell <github.cm@foell.org>
2012-04-20 09:50:36 -0500

    Initial check-in

---
+++ auto-thickbox-gallery.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: Auto Thickbox Gallery
+Description: Creates a thickbox gallery out of media in a post
+Author: Justin Foell
+Version: 1.0
+Author URI: http://www.foell.org/justin
+*/
+define( 'TBGAL_URL', plugins_url( '/',  __FILE__ ) );
+
+class AutoThickboxGallery {
+
+	public function hookDebug( $name ) {
+		echo "<!-- HOOK {$name} -->\n";
+	}
+  	
+	public function onInit() {
+		add_filter( 'image_send_to_editor', array( $this, 'filterImageSend' ), 10, 8 );
+		add_action( 'the_post', array( $this, 'onThePost' ) );
+		add_filter( 'mce_external_plugins', array( $this, 'filterExtMCEPlugins' ) );
+		//add_action( 'all', array( $this, 'hookDebug' ) );
+        //add_filter( 'all', array( $this, 'hookDebug' ) );
+	}
+
+	public function filterExtMCEPlugins( $plugins ) {
+		$plugins['autotbgallery'] = TBGAL_URL . 'js/editor_plugin.js';
+		return $plugins;
+	}
+	
+	public function filterImageSend( $html, $id, $caption, $title, $align, $url, $size, $alt ) {
+		$post = get_post( $id );
+		$title = $caption ? "title=\"{$caption}\" " : '';
+		return str_replace( '<a href', "<a class=\"thickbox\" rel=\"gallery-{$post->post_parent}\" {$title}href", $html );
+	}
+	
+	public function onThePost( $post ) {
+		if ( preg_match( '/<a class="thickbox"[\s]+[^>]*?rel=[\'"]gallery-/', $post->post_content ) ) {
+			wp_enqueue_style( 'thickbox' );
+			wp_enqueue_script( 'thickbox' );
+		}
+	}
+	
+	public function filterImageURL( $html, $src, $alt, $align ) {
+		$stuff = array( $html, $src, $alt, $align );
+		return $html;
+	}	
+}
+
+$tbgal_plugin = new AutoThickboxGallery();
+add_action( 'init', array( $tbgal_plugin, 'onInit' ) );
