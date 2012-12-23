a9644029f3d3940feace3f1b1983cb4217a31890
Justin Foell <justin@9seeds.com>
2012-12-23 10:02:42 -0600

    Cleaned up unused code

--- auto-thickbox-gallery.php
+++ auto-thickbox-gallery.php
@@ -6,24 +6,17 @@ Author: Justin Foell
 Version: 1.1
 Author URI: http://www.foell.org/justin
 */
-define( 'TBGAL_URL', plugins_url( '/',  __FILE__ ) );
 
 class AutoThickboxGallery {
-
-	public function hookDebug( $name ) {
-		echo "<!-- HOOK {$name} -->\n";
-	}
   	
 	public function onInit() {
 		add_filter( 'image_send_to_editor', array( $this, 'filterImageSend' ), 10, 8 );
 		add_action( 'the_post', array( $this, 'onThePost' ) );
 		add_filter( 'mce_external_plugins', array( $this, 'filterExtMCEPlugins' ) );
-		//add_action( 'all', array( $this, 'hookDebug' ) );
-        //add_filter( 'all', array( $this, 'hookDebug' ) );
 	}
 
 	public function filterExtMCEPlugins( $plugins ) {
-		$plugins['autotbgallery'] = TBGAL_URL . 'js/editor_plugin.js';
+		$plugins['autotbgallery'] = plugins_url( '/js/editor_plugin.js',  __FILE__ );
 		return $plugins;
 	}
 	
@@ -38,11 +31,6 @@ class AutoThickboxGallery {
 			wp_enqueue_script( 'thickbox' );
 		}
 	}
-	
-	public function filterImageURL( $html, $src, $alt, $align ) {
-		$stuff = array( $html, $src, $alt, $align );
-		return $html;
-	}	
 }
 
 $tbgal_plugin = new AutoThickboxGallery();
